3b3a9b91ce4389a1723bf0e4deecce423f92e401
Nate Beaty <nate@firebellydesign.com>
2018-10-16 14:06:43 -0500

    use npx for gulp commands

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,17 +58,13 @@ require "#{fetch(:local_abs_path)}/config/webfaction.rb"
 namespace :deploy do
   task :compile_assets do
     run_locally do
-      within fetch(:local_theme_path) do
-        execute :gulp, '--production'
-      end
+      execute "cd #{fetch(:local_theme_path)} && npx gulp --production"
     end
   end
 
   task :ungulp do
     run_locally do
-      within fetch(:local_theme_path) do
-        execute :gulp, '--development'
-      end
+      execute "cd #{fetch(:local_theme_path)} && npx gulp --development"
     end
   end
 
